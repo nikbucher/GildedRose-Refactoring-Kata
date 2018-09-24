08ec3fbf27dff5804d5ad0720dadc47425f68699
Niklaus Bucher <niklaus@bucher.io>
2018-09-24 13:29:31 +0200

    r simplify and remove unneeded if statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,71 +23,31 @@ class GildedRose {
                 }
             } else {
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (true) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                            if (true) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
 
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.sellIn = item.sellIn - 1;
-                    }
+                    item.sellIn = item.sellIn - 1;
 
                     if (item.sellIn < 0) {
-                        if (true) {
-                            item.quality = 0;
-                        } else {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        }
+                        item.quality = 0;
                     }
                 } else {
-                    if (false) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-
-                            if (false) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
                         }
                     }
 
@@ -96,13 +56,9 @@ class GildedRose {
                     }
 
                     if (item.sellIn < 0) {
-                        if (false) {
-                            item.quality = 0;
-                        } else {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     }
