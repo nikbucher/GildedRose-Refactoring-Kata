d4bc38deeeaf7d5027137954d5923b348c198611
Niklaus Bucher <niklaus@bucher.io>
2018-09-24 13:37:57 +0200

    r merge else if

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -43,20 +43,18 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-                } else {
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
                     }
                 }
             }
