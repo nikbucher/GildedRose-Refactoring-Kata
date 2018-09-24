fa18298749d47f21361f717e493f0608102bf600
Niklaus Bucher <niklaus@bucher.io>
2018-09-24 13:34:09 +0200

    r extract method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -44,22 +44,26 @@ class GildedRose {
                     item.quality = 0;
                 }
             } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+                sub(item);
+            }
+        }
+    }
 
+    public void sub(Item item) {
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality = item.quality - 1;
+            }
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+                    item.quality = item.quality - 1;
                 }
             }
         }
